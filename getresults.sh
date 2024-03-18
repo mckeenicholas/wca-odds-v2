93023c5e699b7708ed39967fa9151b5a46433762
Nicholas McKee <nmsmckee@gmail.com>
2024-03-17 22:18:20 -0400

    fixed shell script

--- getresults.sh
+++ getresults.sh
@@ -1,5 +1,17 @@
-curl -o wca_results.zip https://s3.us-west-2.amazonaws.com/assets.worldcubeassociation.org/export/results/WCA_export063_20240303T000038Z.tsv.zip
-unzip wca_results.zip
-rm wca_results.zip
-python droprows.py
-rm -rf wca_results/*.tsv
+# Send a HEAD request and capture the location header
+location=$(curl -s -I https://www.worldcubeassociation.org/export/results/WCA_export.tsv | grep -i location)
+url=$(echo $location | awk '{print $2}' | tr -d '\r')
+
+# Specify the output directory and download the file
+output_dir="results_dump"
+mkdir -p "$output_dir"
+curl -o "$output_dir/wca_results.zip" "$url"
+
+# Unzip the downloaded file
+unzip "$output_dir/wca_results.zip" -d "$output_dir"
+
+# Run your script to process the files
+python3 droprows.py "$output_dir"/WCA_export_Competitions.tsv "$output_dir"/WCA_export_Results.tsv "$output_dir"/Competitions.csv "$output_dir"/Results.csv
+
+# Remove the downloaded zip file and extracted files
+rm -f "$output_dir/wca_results.zip" "$output_dir"/*.tsv "$output_dir"/metadata.json "$output_dir"/README.md
--- import.sql
+++ import.sql
@@ -1,5 +1,5 @@
 SET SEARCH_PATH to wca_results
 
-\copy Competitions (id, year, month, day) FROM 'Competitions.csv' DELIMITER ',' CSV HEADER; 
+\copy Competitions (id, year, month, day) FROM 'results_dump/Competitions.csv' DELIMITER ',' CSV HEADER; 
 
-\copy Results (competitionId, eventId, roundTypeId, personName, personId, formatId, value1, value2, value3, value4, value5) FROM 'Results.csv' DELIMITER ',' CSV HEADER; 
+\copy Results (competitionId, eventId, roundTypeId, personName, personId, formatId, value1, value2, value3, value4, value5) FROM 'results_dump/Results.csv' DELIMITER ',' CSV HEADER; 
